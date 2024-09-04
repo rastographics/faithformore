d44f853d4a8d618f5535f5125aa4c88f97f2dc01
Joel Szwarga <3922354+rastographics@users.noreply.github.com>
2024-09-04 10:05:46 -0500

    Update chapter.php

--- site/templates/chapter.php
+++ site/templates/chapter.php
@@ -27,28 +27,28 @@
                 <img src="https://images.unsplash.com/photo-1521747116042-5a810fda9664?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&q=80&w=1080" alt="Beautiful Landscape" class="w-full h-full object-cover">
             </a>
             <div class="p-4">
-                <h3 class="text-xl font-semibold">Beautiful Landscape</h3>
-                <p class="text-sm mt-2">A stunning sunset landscape photo from Unsplash.</p>
+                <h3 class="text-xl font-semibold">Sample Image</h3>
+                <p class="text-sm mt-2">This is just until we get more images. Click to see full size.</p>
             </div>
         </div>
 
         <!-- Portfolio Item 2: Embedded Vimeo Video -->
         <div class="bg-gray-800 rounded-lg shadow-lg overflow-hidden">
-            <iframe src="https://player.vimeo.com/video/76979871" class="w-full h-64" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            <iframe src="https://player.vimeo.com/video/613787666" class="w-full h-64" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
             <div class="p-4">
-                <h3 class="text-xl font-semibold">Creative Short Film</h3>
-                <p class="text-sm mt-2">A short film embedded from Vimeo showcasing creative storytelling.</p>
+                <h3 class="text-xl font-semibold">40 Years: Keith Gomez/h3>
+                <p class="text-sm mt-2">A description about this awesome video.</p>
             </div>
         </div>
 
         <!-- Portfolio Item 3: Embedded Audio from SoundCloud -->
         <div class="bg-gray-800 rounded-lg shadow-lg overflow-hidden">
             <div class="p-4">
-                <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/340195339&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+                <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/310170773&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
             </div>
             <div class="p-4">
-                <h3 class="text-xl font-semibold">Relaxing Music</h3>
-                <p class="text-sm mt-2">An embedded audio track from SoundCloud with relaxing music.</p>
+                <h3 class="text-xl font-semibold">Another Star In Glory</h3>
+                <p class="text-sm mt-2">An embedded sermon that will be working.</p>
             </div>
         </div>
 
